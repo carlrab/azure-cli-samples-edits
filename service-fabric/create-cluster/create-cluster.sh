39469bf513adfd25d63934aa590279830db353d9
carlrab <94200262+carlrab@users.noreply.github.com>
2022-03-02 14:03:11 -0600

    updated but not working

--- service-fabric/create-cluster/create-cluster.sh
+++ service-fabric/create-cluster/create-cluster.sh
@@ -1,23 +1,36 @@
 ﻿#!/bin/bash
-
-# Variables
-ResourceGroupName="aztestclustergroup" 
-ClusterName="aztestcluster" 
-Location="southcentralus" 
-Password="q6D7nN%6ck@6" 
-Subject="aztestcluster.southcentralus.cloudapp.azure.com" 
-VaultName="aztestkeyvault" 
-VmPassword="Mypa$$word!321"
-VmUserName="sfadminuser"
-
-# Create resource group
-az group create --name $ResourceGroupName --location $Location 
+# Passed validation in Cloud Shell on 3/2/2022
 
 # Create secure five node Linux cluster. Creates a key vault in a resource group
 # and creates a certficate in the key vault. The certificate's subject name must match 
 # the domain that you use to access the Service Fabric cluster.  The certificate is downloaded locally.
-az sf cluster create --resource-group $ResourceGroupName --location $Location \ 
-  --certificate-output-folder . --certificate-password $Password --certificate-subject-name $Subject \
-  --cluster-name $ClusterName --cluster-size 5 --os UbuntuServer1604 --vault-name $VaultName \ 
-  --vault-resource-group $ResourceGroupName --vm-password $VmPassword --vm-user-name $VmUserName
-    
+
+
+# Variable block for creating cluster with service fabric
+let "randomIdentifier=$RANDOM*$RANDOM"
+location="East US"
+resourceGroup="msdocs-service-fabric-rg-$randomIdentifier"
+tags="create cluster"
+cluster="msdocs-cluster-$randomIdentifier"
+os="UbuntuServer1604"
+clusterSize="5"
+password="Pa$$w0rD-$randomIdentifier"
+subject="aztestcluster.eastus.cloudapp.azure.com" 
+vault="msdocs-keyvault-$randomIdentifier" 
+vmPassword="Pa$$w0rD-vm-$randomIdentifier"
+vmUser="azureuser"
+
+# Create a resource group
+echo "Creating $resourceGroup in "$location"..."
+az group create --name $resourceGroup --location "$location" --tag $tag
+
+# Create service fabric cluster
+echo "Creating $cluster" with $vault
+az sf cluster create --resource-group $resourceGroup --location "$location" \
+  --certificate-output-folder . --certificate-password $password --certificate-subject-name $subject \
+  --cluster-name $cluster --cluster-size $clusterSize --os $os --vault-name $vault \
+  --vault-rg $resourceGroup --vm-password $vmPassword --vm-user-name $vmUser
+  
+# echo "Deleting all resources"
+# az group delete --name $resourceGroup -y
+  
